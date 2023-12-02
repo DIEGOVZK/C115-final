c0e6671733176735652c17b904f5c63faa38f5d1
paulolopestech <phlj.junior@gmail.com>
2023-12-01 23:57:06 -0300

    :construction: setup backend

---
+++ backend/src/mqtt.client.ts
@@ -0,0 +1,41 @@
+import * as mqtt from 'mqtt';
+import { WebSocketServer } from './websocket.server';
+
+export class MqttClient {
+  private client: any;
+
+  constructor(private brokerUrl: string, wsServer: WebSocketServer) {
+    this.client = mqtt.connect(brokerUrl);
+
+    this.client.on('connect', () => {
+      console.log('Conectado ao broker MQTT');
+    });
+
+    this.client.on('message', (topic: any, message: any) => {
+      if (topic === '68:C6:3A:88:CA:E9') {
+        console.log(`Mensagem recebida no tópico '${topic}': ${message.toString()}`);
+        wsServer.broadcastMessage(message.toString());
+      }
+    });
+
+    this.client.on('close', () => {
+      console.log('Conexão MQTT fechada');
+    });
+
+    this.client.on('error', (error: any) => {
+      console.error(`Erro no cliente MQTT: ${error}`);
+    });
+  }
+
+  public publishMessage(topic: string, message: string): void {
+    this.client.publish(topic, message);
+  }
+
+  public subscribeToTopic(topic: string): void {
+    this.client.subscribe(topic, (err: any) => {
+      if (!err) {
+        console.log(`Inscrito no tópico '${topic}'`);
+      }
+    });
+  }
+}
