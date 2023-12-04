e2a61f0f65a22621692789ce8b9f9e77b6435e01
paulolopestech <phlj.junior@gmail.com>
2023-12-04 01:28:46 -0300

    :recycle: refactor routes

--- dashboard/src/axios.client.js
+++ dashboard/src/axios.client.js
@@ -4,26 +4,12 @@ const client = axios.create({
     baseURL: 'http://localhost:3005'
   });
 
-export const updateAccX = async (limits) => {
-    await client.post('/accx/limits/set', limits);
+export const updateLimit = async (limits) => {
+    await client.post('/limits/set', {...limits});
 }
 
-export const updateAccY = async (limits) => {
-    await client.post('/accx/limits/set', limits);
-}
 
-export const updateAccZ = async (limits) => {
-    await client.post('/accx/limits/set', limits);
-}
-
-export const updateGyroX = async (limits) => {
-    await client.post('/gyrox/limits/set', limits);
-}
-
-export const updateGyroY = async (limits) => {
-    await client.post('/gyroy/limits/set', limits);
-}
-
-export const updateGyroZ = async (limits) => {
-    await client.post('/gyroz/limits/set', limits);
+export const registerEmail = async (email) => {
+    console.log('SE:::', email)
+    await client.post('/email/register', {email});
 }
